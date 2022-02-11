b31603df5c37fa741bab19892a6216a28af60507
TheSKY <XIE_hao_SKY@126.com>
2022-02-11 12:43:50 +0800

    上传代码

---
+++ 1.10/1.10/test.c
@@ -0,0 +1,20 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//int main()
+// {
+//	int a = 20;
+//	int b = -10;
+//	printf("%p", a);
+//	printf("%p", b);
+//	return 0;
+//}
+
+#include<stdlib.h>
+
+int main()
+{
+	int a = 0x11223344;
+	printf("%p", a);
+	return 0;
+}

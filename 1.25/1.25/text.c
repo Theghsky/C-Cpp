230d013ec7f9e0d065cfc74dcbb74b5203cd8c86
TheSKY <XIE_hao_SKY@126.com>
2022-02-14 16:17:44 +0800

    上传代码

---
+++ 1.25/1.25/text.c
@@ -0,0 +1,21 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include <stdio.h>
+void test1(int arr[])
+{
+	printf("%d\n", sizeof(arr));//(2)
+}
+void test2(char ch[])
+{
+	printf("%d\n", sizeof(ch));//(4)
+}
+int main()
+{
+	int arr[10] = { 0 };
+	char ch[10] = { 0 };
+	printf("%d\n", sizeof(arr));//(1)
+	printf("%d\n", sizeof(ch));//(3)
+	test1(arr);
+	test2(ch);
+	return 0;
+}

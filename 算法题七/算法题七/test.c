c315339174ee834a2cce6000f5ffc702803bb954
TheSKY <XIE_hao_SKY@126.com>
2022-03-21 23:25:51 +0800

    上传代码

---
+++ 算法题七/算法题七/test.c
@@ -0,0 +1,144 @@
+﻿//题一：
+//给你一个链表的头节点 head ，判断链表中是否有环。
+//
+//如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。注意：pos 不作为参数进行传递 。仅仅是为了标识链表的实际情况。
+//
+//如果链表中存在环 ，则返回 true 。 否则，返回 false 。
+//
+//难度：简单
+//
+//示例 1：
+//
+//来源：力扣（LeetCode）
+//链接：https ://leetcode-cn.com/problems/linked-list-cycle
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+//输入：head = [3, 2, 0, -4], pos = 1
+//输出：true
+//解释：链表中有一个环，其尾部连接到第二个节点。
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+bool hasCycle(struct ListNode* head)
+{
+    struct ListNode* slow = head, * fast = head;
+
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+//题二
+//给定一个头结点为 head 的非空单链表，返回链表的中间结点。
+//
+//如果有两个中间结点，则返回第二个中间结点。
+//难度：简单
+//示例 1：
+//
+//输入：[1, 2, 3, 4, 5]
+//输出：此列表中的结点 3 (序列化形式：[3, 4, 5])
+//返回的结点值为 3 。(测评系统对该结点序列化表述是[3, 4, 5])。
+//注意，我们返回了一个 ListNode 类型的对象 ans，这样：
+//ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, 以及 ans.next.next.next = NULL.
+//
+//来源：力扣（LeetCode）
+//链接：https ://leetcode-cn.com/problems/middle-of-the-linked-list
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* middleNode(struct ListNode* head)
+{
+    struct ListNode* slow=head,*fast=head;
+    while(fast && fast->next)
+    {
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    return slow;
+}
+
+//题三
+//将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+
+//示例 1：
+//难度：简单
+//
+//输入：l1 = [1, 2, 4], l2 = [1, 3, 4]
+//输出：[1, 1, 2, 3, 4, 4]
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2)
+{
+    if (l1 == NULL)
+        return l2;
+    if (l2 == NULL)
+        return l1;
+
+    struct ListNode* head = NULL, * tail = NULL;
+    while (l1 != NULL && l2 != NULL)
+    {
+        if (l1->val < l2->val)
+        {
+            if (tail == NULL)
+            {
+                head = tail = l1;
+            }
+            else
+            {
+                tail->next = l1;
+                tail = tail->next;
+            }
+            l1 = l1->next;
+        }
+        else
+        {
+            if (tail == NULL)
+            {
+                head = tail = l2;
+            }
+            else
+            {
+                tail->next = l2;
+                tail = tail->next;
+            }
+
+            l2 = l2->next;
+        }
+    }
+
+    if (l1)
+        tail->next = l1;
+    if (l2)
+        tail->next = l2;
+
+    return head;
+}

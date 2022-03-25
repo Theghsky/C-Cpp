fcb9d60fe23e878715d824209c1f59618eeffb4a
TheSKY <XIE_hao_SKY@126.com>
2022-03-25 23:57:41 +0800

    上传代码

---
+++ 算法题十/算法题十/test.c
@@ -0,0 +1,247 @@
+﻿//请你仅使用两个队列实现一个后入先出（LIFO）的栈，并支持普通栈的全部四种操作（push、top、pop 和 empty）。
+//
+//实现 MyStack 类：
+//
+//void push(int x) 将元素 x 压入栈顶。
+//int pop() 移除并返回栈顶元素。
+//int top() 返回栈顶元素。
+//boolean empty() 如果栈是空的，返回 true ；否则，返回 false 。
+//
+//
+//注意：
+//
+//你只能使用队列的基本操作 —— 也就是 push to back、peek / pop from front、size 和 is empty 这些操作。
+//你所使用的语言也许不支持队列。 你可以使用 list （列表）或者 deque（双端队列）来模拟一个队列 , 只要是标准的队列操作即可。
+//
+//
+//示例：
+//
+//输入：
+//["MyStack", "push", "push", "top", "pop", "empty"]
+//[[], [1], [2], [], [], []]
+//输出：
+//[null, null, null, 2, 2, false]
+//
+//解释：
+//MyStack myStack = new MyStack();
+//myStack.push(1);
+//myStack.push(2);
+//myStack.top(); // 返回 2
+//myStack.pop(); // 返回 2
+//myStack.empty(); // 返回 False
+//
+//来源：力扣（LeetCode）
+//链接：https ://leetcode-cn.com/problems/implement-stack-using-queues
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+//难度：简单
+
+typedef int QDataType;
+
+typedef struct QueueNode
+{
+	struct QueueNode* next;
+	QDataType data;
+}QNode;
+
+typedef struct Queue
+{
+	QNode* head;
+	QNode* tail;
+}Queue;
+
+void QueueInit(Queue* pq);
+void QueueDestory(Queue* pq);
+
+// 队尾入
+void QueuePush(Queue* pq, QDataType x);
+// 队头出
+void QueuePop(Queue* pq);
+QDataType QueueFront(Queue* pq);
+QDataType QueueBack(Queue* pq);
+int QueueSize(Queue* pq);
+bool QueueEmpty(Queue* pq);
+
+void QueueInit(Queue* pq)
+{
+	assert(pq);
+	pq->head = pq->tail = NULL;
+}
+
+void QueueDestory(Queue* pq)
+{
+	assert(pq);
+
+	QNode* cur = pq->head;
+	while (cur)
+	{
+		QNode* next = cur->next;
+		free(cur);
+		cur = next;
+	}
+
+	pq->head = pq->tail = NULL;
+}
+
+// 队尾入
+void QueuePush(Queue* pq, QDataType x)
+{
+	assert(pq);
+	QNode* newnode = (QNode*)malloc(sizeof(QNode));
+	if (newnode == NULL)
+	{
+		printf("malloc fail\n");
+		exit(-1);
+	}
+
+	newnode->data = x;
+	newnode->next = NULL;
+
+	if (pq->tail == NULL)
+	{
+		pq->head = pq->tail = newnode;
+	}
+	else
+	{
+		pq->tail->next = newnode;
+		pq->tail = newnode;
+	}
+}
+
+// 队头出
+void QueuePop(Queue* pq)
+{
+	assert(pq);
+	assert(pq->head);
+
+	// 1、一个
+	// 2、多个
+	if (pq->head->next == NULL)
+	{
+		free(pq->head);
+		pq->head = pq->tail = NULL;
+	}
+	else
+	{
+		QNode* next = pq->head->next;
+		free(pq->head);
+		pq->head = next;
+	}
+}
+
+
+QDataType QueueFront(Queue* pq)
+{
+	assert(pq);
+	assert(pq->head);
+
+	return pq->head->data;
+}
+
+QDataType QueueBack(Queue* pq)
+{
+	assert(pq);
+	assert(pq->head);
+
+	return pq->tail->data;
+}
+
+int QueueSize(Queue* pq)
+{
+	assert(pq);
+	int size = 0;
+	QNode* cur = pq->head;
+	while (cur)
+	{
+		++size;
+		cur = cur->next;
+	}
+
+	return size;
+}
+
+bool QueueEmpty(Queue* pq)
+{
+	assert(pq);
+	return pq->head == NULL;
+}
+
+typedef struct
+{
+	Queue q1;
+	Queue q2;
+} MyStack;
+
+
+MyStack* myStackCreate()
+{
+	MyStack* ps = (MyStack*)malloc(sizeof(MyStack));
+	if (ps == NULL)
+	{
+		printf("malloc fail\n");
+		exit(-1);
+	}
+	QueueInit(&ps->q1);
+	QueueInit(&ps->q2);
+
+	return ps;
+}
+
+void myStackPush(MyStack* obj, int x)
+{
+	if (!QueueEmpty(&obj->q1))
+	{
+		QueuePush(&obj->q1, x);
+	}
+	else
+	{
+		QueuePush(&obj->q2, x);
+	}
+}
+
+int myStackPop(MyStack* obj)
+{
+	Queue* emptyQ = &obj->q1;
+	Queue* nonemptyQ = &obj->q2;
+	if (!QueueEmpty(&obj->q1))
+	{
+		emptyQ = &obj->q2;
+		nonemptyQ = &obj->q1;
+	}
+	//倒数据
+	while (QueueSize(nonemptyQ) > 1)
+	{
+		QueuePush(emptyQ, QueueFront(nonemptyQ));
+		QueuePop(nonemptyQ);
+	}
+
+	int top = QueueFront(nonemptyQ);
+	QueuePop(nonemptyQ);
+
+	return top;
+}
+
+int myStackTop(MyStack* obj)
+{
+	if (!QueueEmpty(&obj->q1))
+	{
+		return QueueBack(&obj->q1);
+	}
+	else
+	{
+		return QueueBack(&obj->q2);
+	}
+}
+
+bool myStackEmpty(MyStack* obj)
+{
+	return QueueEmpty(&obj->q1) && QueueEmpty(&obj->q2);
+}
+
+void myStackFree(MyStack* obj)
+{
+	QueueDestory(&obj->q1);
+	QueueDestory(&obj->q2);
+
+	free(obj);
+}

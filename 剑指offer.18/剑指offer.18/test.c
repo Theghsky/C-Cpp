f32a448108ad7fcb7e2a4622a71d07a4f47a513d
TheSKY <XIE_hao_SKY@126.com>
2022-05-03 17:01:01 +0800

    上传代码

---
+++ 剑指offer.18/剑指offer.18/test.c
@@ -0,0 +1,23 @@
+//输入数字 n，按顺序打印出从 1 到最大的 n 位十进制数。比如输入 3，则打印出 1、2、3 一直到最大的 3 位数 999。
+//
+//示例 1:
+//
+//输入: n = 1
+//输出 : [1, 2, 3, 4, 5, 6, 7, 8, 9]
+//
+//来源：力扣（LeetCode）
+//链接：https ://leetcode-cn.com/problems/da-yin-cong-1dao-zui-da-de-nwei-shu-lcof
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+int* printNumbers(int n, int* returnSize)
+{
+    int max = pow(10, n);
+    int* ret = (int*)malloc(sizeof(int) * (max - 1));
+    *returnSize = max - 1;
+    int size = 0;
+    for (int i = 1; i < max; ++i)
+    {
+        ret[size++] = i;
+    }
+    return ret;
+}

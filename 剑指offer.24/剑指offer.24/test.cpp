62f4ae5e482f7dd710801bc6ef11d6631d025d8e
TheSKY <XIE_hao_SKY@126.com>
2022-05-11 15:34:52 +0800

    上传代码

---
+++ 剑指offer.24/剑指offer.24/test.cpp
@@ -0,0 +1,44 @@
+输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+
+示例1：
+
+输入：1->2->4, 1->3->4
+输出：1->1->2->3->4->4
+
+来源：力扣（LeetCode）
+链接：https ://leetcode.cn/problems/he-bing-liang-ge-pai-xu-de-lian-biao-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+    class Solution {
+    public:
+        ListNode* mergeTwoLists(ListNode* l1, ListNode* l2)
+        {
+            ListNode* newHead = new ListNode(-1);
+
+            ListNode* cur = newHead;
+            while (l1 != nullptr && l2 != nullptr)
+            {
+                if (l1->val < l2->val)
+                {
+                    cur->next = l1;
+                    l1 = l1->next;
+                }
+                else
+                {
+                    cur->next = l2;
+                    l2 = l2->next;
+                }
+                cur = cur->next;
+            }
+            cur->next = l1 == nullptr ? l2 : l1;
+            return newHead->next;
+        }
+};

c2ea6950feadecf2c3865d5d817dba4032395378
TheSKY <XIE_hao_SKY@126.com>
2022-05-14 17:01:32 +0800

    上传代码

---
+++ 剑指offer.26/剑指offer.26/test.c
@@ -0,0 +1,46 @@
+﻿//请完成一个函数，输入一个二叉树，该函数输出它的镜像。
+//
+//例如输入：
+//
+//4
+/// \
+//2     7
+/// \ / \
+//1   3 6   9
+//镜像输出：
+//
+//4
+/// \
+//7     2
+/// \ / \
+//9   6 3   1
+//
+//示例 1：
+//
+//输入：root = [4, 2, 7, 1, 3, 6, 9]
+//输出：[4, 7, 2, 9, 6, 3, 1]
+//
+//来源：力扣（LeetCode）
+//链接：https ://leetcode.cn/problems/er-cha-shu-de-jing-xiang-lcof
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+struct TreeNode* mirrorTree(struct TreeNode* root)
+{
+    if (root == NULL)
+        return NULL;
+    struct TreeNode* left = mirrorTree(root->left);
+    struct TreeNode* right = mirrorTree(root->right);
+    root->left = right;
+    root->right = left;
+    return root;
+}

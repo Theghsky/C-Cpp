a6f8a520eb7c6f6fc693f6925fcfb7a328330c0f
TheSKY <XIE_hao_SKY@126.com>
2022-05-28 01:36:08 +0800

    上传代码

---
+++ 剑指offer.36/剑指offer.36/test.c
@@ -0,0 +1,37 @@
+//在一个 m* n 的棋盘的每一格都放有一个礼物，每个礼物都有一定的价值（价值大于 0）。你可以从棋盘的左上角开始拿格子里的礼物，并每次向右或者向下移动一格、直到到达棋盘的右下角。给定一个棋盘及其上面的礼物的价值，请计算你最多能拿到多少价值的礼物？
+//
+//示例 1:
+//
+//输入:
+//[
+//	[1, 3, 1],
+//	[1, 5, 1],
+//	[4, 2, 1]
+//]
+//输出 : 12
+//解释 : 路径 1→3→5→2→1 可以拿到最多价值的礼物
+//
+//来源：力扣（LeetCode）
+//链接：https ://leetcode.cn/problems/li-wu-de-zui-da-jie-zhi-lcof
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+int maxValue(int** grid, int gridSize, int* gridColSize)
+{
+    int i, j, sum;
+    for (i = 0; i < gridSize; i++)
+    {
+        for (j = 0; j < *gridColSize; j++)
+        {
+            if (i == 0 && j == 0)
+                continue;
+            else if (j == 0)
+                grid[i][j] += grid[i - 1][j];
+            else if (i == 0)
+                grid[i][j] += grid[i][j - 1];
+            else
+                grid[i][j] += (grid[i][j - 1] > grid[i - 1][j]) ? grid[i][j - 1] : grid[i - 1][j];
+        }
+    }
+    return grid[gridSize - 1][*gridColSize - 1];
+}
+

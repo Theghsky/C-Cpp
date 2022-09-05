54df5c1f9bab7aa1d87cd6aa7aa3243bb19af243
TheSKY <XIE_hao_SKY@126.com>
2022-09-05 23:34:52 +0800

    上传代码

---
+++ 9.5/test2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include<stdlib.h>
+
+int main(void)
+{
+    pid_t pid;
+    int i,n=5;
+    for(i=0;i<n;i++){
+	if((pid=fork())<0){
+	    perror("fork:");
+	    exit(-1);
+	}else if(pid>0){
+	    printf("%d %d\n",getpid(),getppid());
+	    break;
+	}
+    }
+
+    for(i=0;i<n;i++)
+	wait(NULL);
+    return 0;
+}
